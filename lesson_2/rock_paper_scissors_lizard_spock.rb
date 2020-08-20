b2a6f9a848235a7f856a60e1a0be991256b0db7d
Maya Angelou <MayaAngelou@poets.com>
2020-08-20 19:22:49 +0800

    Refactored RPSLS

--- lesson_2/rock_paper_scissors_lizard_spock.rb
+++ lesson_2/rock_paper_scissors_lizard_spock.rb
@@ -18,7 +18,58 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
-def total_score(player, computer, scoreboard)
+def clear_screen
+  print "\033[2J\033[H"
+end
+
+def display_welcome
+  prompt("Welcome to Rock, Paper, Sissors, Lizard, Spock!")
+  prompt("Can you beat the computer 5 times?")
+end
+
+def display_choice_prompt
+  choice_prompt = <<-MSG
+
+  Pick one:
+
+  r = rock
+  p = paper
+  sc = scissors
+  l = lizard
+  sp = spock
+  MSG
+end
+
+def get_player_choice(choice)
+  loop do
+    Kernel.puts(display_choice_prompt)
+    choice = Kernel.gets().chomp()
+    if valid_choice?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+  choice
+end
+
+def valid_choice?(choice)
+  VALID_CHOICES.key?(choice) || VALID_CHOICES.value?(choice)
+end
+
+def convert_player_choice?(choice)
+  if VALID_CHOICES.key?(choice)
+    VALID_CHOICES[choice]
+  else
+    choice
+  end
+end
+
+def get_computer_choice
+  VALID_CHOICES[VALID_CHOICES.keys.sample]
+end
+
+def update_score(player, computer, scoreboard)
   if WINNING_COMBOS[player].include?(computer)
     scoreboard[:player] += 1
   elsif WINNING_COMBOS[computer].include?(player)
@@ -26,7 +77,7 @@ def total_score(player, computer, scoreboard)
   end
 end
 
-def display_results(player, computer)
+def display_who_won?(player, computer)
   if WINNING_COMBOS[player].include?(computer)
     prompt "You Won!"
   elsif WINNING_COMBOS[computer].include?(player)
@@ -44,58 +95,57 @@ def display_grand_winner(scoreboard)
   end
 end
 
-scoreboard = { player: 0, computer: 0 }
+def display_choices(choice, computer_choice)
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+end
 
-prompt("Welcome to Rock, Paper, Sissors, Lizard, Spock!")
-prompt("Can you beat the computer 5 times?")
+def display_score(scoreboard)
+  prompt("score: you:#{scoreboard[:player]} computer:#{scoreboard[:computer]}")
+end
 
-loop do
-  choice = ''
+def grand_winner?(scoreboard)
+  if scoreboard[:computer] == 5 || scoreboard[:player] == 5
+    prompt("Grand winner goes to...")
+    display_grand_winner(scoreboard)
+    scoreboard[:computer] = 0
+    scoreboard[:player] = 0
+  end
+end
+
+def play_again?
   loop do
-    choice_prompt = <<-MSG
-
-        Pick one:
-
-        r = rock
-        p = paper
-        sc = scissors
-        l = lizard
-        sp = spock
-        MSG
-
-    prompt(choice_prompt)
-
-    choice = Kernel.gets().chomp()
-
-    if VALID_CHOICES.key?(choice) || VALID_CHOICES.value?(choice)
+    prompt("Play again? Type y/yes or n/no")
+    answer = gets().chomp().downcase
+    if answer == 'n' || answer == 'no'
+      prompt("Thank you for playing. Good bye!")
+      Kernel.exit()
+    elsif answer == 'y' || answer == 'yes'
       break
     else
       prompt("That's not a valid choice.")
     end
   end
-
-  computer_choice = VALID_CHOICES[VALID_CHOICES.keys.sample]
-
-  if VALID_CHOICES.key?(choice)
-    choice = VALID_CHOICES[choice]
-  end
-
-  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
-
-  display_results(choice, computer_choice)
-
-  total_score(choice, computer_choice, scoreboard)
-  prompt("score: you:#{scoreboard[:player]} computer:#{scoreboard[:computer]}")
-
-  if scoreboard[:computer] == 5 || scoreboard[:player] == 5
-    prompt("Grand winner goes to...")
-    display_grand_winner(scoreboard)
-    break
-  end
-
-  prompt("Press any key to play again. Type 'n' to exit")
-  answer = gets().chomp()
-  break if answer.downcase.start_with?('n')
 end
 
-prompt("Thank you for playing. Good bye!")
+scoreboard = { player: 0, computer: 0 }
+
+loop do
+  clear_screen
+  display_welcome
+
+  choice = get_player_choice(choice)
+
+  choice = convert_player_choice?(choice)
+
+  computer_choice = get_computer_choice
+
+  display_choices(choice,computer_choice)
+  display_who_won?(choice, computer_choice)
+
+  update_score(choice, computer_choice, scoreboard)
+  display_score(scoreboard)
+
+  grand_winner?(scoreboard)
+
+  play_again?
+end
